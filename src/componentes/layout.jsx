f78ac5f08efb06b734f2bb5b66ed4887a257a8fd
Anuja-More <anujamore2017@gmail.com>
2023-05-04 11:21:55 +0530

    toastprovider

--- src/componentes/layout.jsx
+++ src/componentes/layout.jsx
@@ -1,4 +1,4 @@
-import ToasterProvider from '@/providers/toasterProvider'
+import ToasterProvider from '../providers/ToasterProvider';
 import React from 'react'
 import RegisterModal from './modals/RegisterModal'
 import Navbar from './Navbar/Navbar'
